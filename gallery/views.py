611d916a5a0901603f6c7b06c601a7242355ea6a
Mathieu SOUM <mathieu.soum@gmail.com>
2014-12-20 16:35:59 +0100

    Renaming according to python good practices.

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,5 @@
 from gallery.forms import ImageForm, AlbumForm
-from gallery.utils import create_thumbnail#, to_regular, to_hd, to_ImageField
+from gallery.utils import create_thumbnail  # , to_regular, to_hd, to_ImageField
 from django.shortcuts import render, redirect, get_object_or_404
 from gallery.models import Album, Image
 from django.utils.text import slugify
@@ -7,17 +7,18 @@ from django.utils.text import slugify
 # Create your views here.
 
 def image_list(request):
-    album_list = Album.objects.all()
-    image_list = Image.objects.all()
+    album_lst = Album.objects.all()
+    image_lst = Image.objects.all()
     return render(request, 'gallery/image_list.html', locals())
-    
+
+
 def image_add(request):
-    album_list = Album.objects.all()
+    album_lst = Album.objects.all()
     if request.method == 'POST':
-        form = ImageForm(request.POST, request.FILES,)
+        form = ImageForm(request.POST, request.FILES, )
         if form.is_valid():
             image_instance = form.save()
-            image_instance.thumbnail = create_thumbnail(image_instance)
+            create_thumbnail(image_instance)
             image_instance.save()
             return redirect('gallery.views.album_detail', image_instance.album.slug)
     else:
@@ -27,35 +28,39 @@ def image_add(request):
             form = ImageForm()
         else:
             album = Album.objects.get(slug=album_slug)
-#             print("album : %s // album slug : %s" % (album_slug, album_slug))
+            # print("album : %s // album slug : %s" % (album_slug, album_slug))
             form = ImageForm(initial={'album': album})
-        
+
     return render(request, 'gallery/image_add.html', locals())
 
+
 def image_del(request, album_slug, image_id):
-        album = get_object_or_404(Album, slug=album_slug)
-        album.image_set.filter(id=image_id).delete()
-        return redirect('gallery.views.album_detail', slug=album_slug)
+    album = get_object_or_404(Album, slug=album_slug)
+    album.image_set.filter(id=image_id).delete()
+    return redirect('gallery.views.album_detail', slug=album_slug)
+
 
 def album_list(request):
-    album_list = Album.objects.all()
+    album_lst = Album.objects.all()
     return render(request, 'gallery/album_list.html', locals())
-    
+
+
 def album_add(request):
     if request.method == 'POST':
         form = AlbumForm(request.POST)
-        
+
         if form.is_valid():
             new_album = form.save(commit=False)
             new_album.slug = slugify(new_album.title)
             new_album.save()
             return redirect('gallery.views.album_list')
-            
+
     else:
         form = AlbumForm()
-        
+
     return render(request, 'gallery/album_add.html', locals())
-        
+
+
 def album_detail(request, slug):
     album = get_object_or_404(Album, slug=slug)
     return render(request, 'gallery/album_detail.html', locals())
