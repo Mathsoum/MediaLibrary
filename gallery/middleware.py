23e9b2a571dcab20f4910d4baa7ceab6b9b28d34
Mathieu SOUM <mathieu.soum@gmail.com>
2015-01-15 14:48:10 +0100

    Sign up now unprotected

--- gallery/middleware.py
+++ gallery/middleware.py
@@ -6,6 +6,7 @@ class GalleryMiddleware(object):
 
     def process_request(self, request):
         if not request.user.is_authenticated():
-            if request.path not in ["/", "/gallery"]:
+            print('User not logged in! Path : %s' % request.path)
+            if request.path not in ['/', '/gallery', '/gallery/user/add']:
                 messages.warning(request, 'You must be logged in to access this page!')
                 return redirect('/')
